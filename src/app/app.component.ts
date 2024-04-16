fba29aea8a5ff90ed324e8a5240f11d6491b6a67
josefandino <josefandinod@gmail.com>
2024-04-16 12:16:54 -0500

    Update app.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AngularModule, MaterialModule } from './shared/modules';
 import { StoreModule } from '@ngrx/store';
-import { authReducer } from './auth/store/auth.reduce';
 import { SidebarComponent } from "./loyouts/sidebar/sidebar.component";
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from "./loyouts/header/header.component";
 import { BuscadorComponent } from "./loyouts/buscador/buscador.component";
 import { HomeComponent } from "./panel/home/home.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [
-        AngularModule,
-        MaterialModule,
-        StoreModule,
-        SidebarComponent,
-        LoginComponent,
-        HeaderComponent,
-        BuscadorComponent,
-        HomeComponent
-    ]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    AngularModule,
+    MaterialModule,
+    StoreModule,
+    SidebarComponent,
+    LoginComponent,
+    HeaderComponent,
+    BuscadorComponent,
+    HomeComponent
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'otros-costos';
+
+  isLogin = true;
+
+  constructor() {}
+
+  ngOnInit() {
+    const ruta = window.location.pathname;
+    console.log(ruta);
+    if (ruta === '/login' || ruta === '/') {
+      this.isLogin = true;
+    } else{
+      this.isLogin = false;
+    }
+  }
 
 
 }
